a764aa91618af856a3e24d2524b6c5039242e905
PaolaDMadd <44216565+PaolaDMadd@users.noreply.github.com>
2019-12-05 12:32:55 +0000

    create buttons
    remove initial button "on-click" and creating additional buttons.

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-const allFourSuit = ["hearts", "spades", "diamonds", "clubs"]; // Sam variable for all suits;
+const allFourSuits = ["hearts", "spades", "diamonds", "clubs"]; // Sam variable for all suits;
 const cardsWrapper = document.querySelector('.cards-wrapper');
 
 const cards = [];
@@ -30,6 +30,19 @@ function createCards() {
 // Function to clear out the initial button and create new buttons to play the game.
 function createButtons() {
   // Your Code
+  makeDisappear = document.getElementById("start-game");
+  shuffleBtn = document.getElementById("Shuffle");
+  showHideBtn = document.getElementById("Show-Hide");
+  magicBtn = document.getElementById("Magic");
+
+  makeDisappear.parentElement.removeChild(makeDisappear);
+  makeDisappear.style.display = "none";
+
+  if (makeDisappear.style.display === "none") {
+    shuffleBtn.classList.remove("invisible");
+    showHideBtn.classList.remove("invisible");
+    magicBtn.classList.remove("invisible");
+  }
 }
 
 // Function to start the game by clearing the wrapper, creating
